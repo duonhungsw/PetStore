a8bd305ae37a576f547b00100561b424830e0098
nguyentct19 <thnguyen1928@gmail.com>
2024-06-08 15:10:02 +0700

    add controller

---
+++ src/java/ControllerN/home/CategoryControl.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package ControllerN.home;
+
+import daos.CategoryDAO;
+import daos.ProductDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import model.Category;
+import model.Product;
+
+/**
+ *
+ * @author thngu
+ */
+@WebServlet(name="CategoryControl", urlPatterns={"/category"})
+public class CategoryControl extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CategoryControl</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CategoryControl at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       
+        String cate_Id = request.getParameter("cid");
+        //da lay dc category id ve roi
+        ProductDAO dao = new ProductDAO();
+        
+        CategoryDAO daos = new CategoryDAO();
+        List<Product> list = dao.getProductByCID(cate_Id);
+        List<Category> listC = dao.getAllCategory();
+        
+        request.setAttribute("listP", list);
+        request.setAttribute("listCC", listC);
+        
+        request.setAttribute("tag", cate_Id);   
+        request.getRequestDispatcher("product.jsp").forward(request, response);
+    }
+    } 
+
